01721ffa08bbe1845943e7ae96ffce08d4556403
KyoungBae <jbs800@naver.com>
2015-10-08 17:44:05 +0900

    First

---
+++ MyApplication/app/src/main/java/com/example/kyoungbae/myapplication/BtnTest.java
@@ -0,0 +1,27 @@
+package com.example.kyoungbae.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class BtnTest extends Activity {
+    class ButtonEvent implements View.OnClickListener{
+        public void onClick(View v){
+            Toast.makeText(getApplicationContext(),"Hello android",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn);
+
+        ButtonEvent buttonclick = new ButtonEvent();
+        btn.setOnClickListener(buttonclick);
+
+    }
+
+}
